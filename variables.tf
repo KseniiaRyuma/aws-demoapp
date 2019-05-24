ab70651410d54622ca266b2437e0a46b5df19f2a
cassidy1 <rcassidy@hashicorp.com>
2019-05-23 19:13:42 -0700

    updating to simple ec2

--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "av_zone" {
-  description = "AWS region"
-  default     = "us-east-1b"
-}
-
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
   default     = "ami-2e1ef954"
