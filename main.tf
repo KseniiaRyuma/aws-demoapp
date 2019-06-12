65ad554266c48b5e483d9a45b34731a7b2258230
cassidy1 <rcassidy@hashicorp.com>
2019-06-12 14:23:27 -0700

    updates

--- main.tf
+++ main.tf
@@ -6,24 +6,14 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "null_resource" "get-go" {
-  provisioner "local-exec" {
-    command = "sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get install golang"
-  }
-}
-
 resource "aws_instance" "demo_ec2" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}b"
   associate_public_ip_address = "true"
-  depends_on = ["null_resource.get-go"]
   tags {
     Name = "${var.name}"
     Owner = "${var.owner}"
     TTL = "${var.ttl}"
-  }
-  provisioner "local-exec" {
-    command = "go run hello-world.go"
   }
 }
