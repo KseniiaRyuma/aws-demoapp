8770142f286497b24d3db5d8b33e908157cc0b9e
Ryan Cassidy <ryancassidy0@gmail.com>
2019-09-17 18:27:41 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo_ec2" {
+  count                       = 2
   ami                         = var.ami_id
   instance_type               = var.instance_type
   availability_zone           = "${var.aws_region}b"
